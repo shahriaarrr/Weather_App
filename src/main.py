79ab6835855c9b76dd2b67b67af8e9efe27fc087
shahriaarrr <shahriaarrr@gmail.com>
2022-06-15 20:56:23 +0430

    add mysql-connector and clean code

--- src/main.py
+++ src/main.py
@@ -1,31 +1,48 @@
 import datetime as dt
 import requests
 import schedule
-#for loading and reading .env file
-import dotenv
+# for loading and reading .env file
+from dotenv import load_dotenv
 import os
+# for working with MYSQL database
+import mysql.connector
 
-#load .env file and read API_KEY from .env file
-dotenv.load_dotenv()
+# connect to MYSQL database
+print("connecting to db...")
+cnx = mysql.connector.connect(user='{username}', password='{password}',
+                              # if your database in your local system you can skip the ((host)) field
+                              host='127.0.0.1',
+                              database='{database name}')
+print("connected to db")
+cursor = cnx.cursor()
+
+# load .env file and read API_KEY from .env file
+load_dotenv()
 API_KEY = os.getenv('API_KEY')
-#add our URL
+
+# add our base URL
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
-#Add a city which weather conditions we want
+
+# Add a city which weather conditions we want
 CITY = "Tehran"
 
+# Convert Kelvin to Celsius
 def kelvin_to_celsius(kelvin):
     celsius = kelvin - 273.15
-
-
     return celsius
 
+# Create a URL to send a request
 url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
 
-def check_weather():
+# our main function
+def main():
+    print("getting information from openweather...")
+    # send request and save json response
     response = requests.get(url).json()
 
+    # get our needed info from response
     temp_kelvin = response['main']['temp']
-    temp_celsius= kelvin_to_celsius(temp_kelvin)
+    temp_celsius = float(f"{kelvin_to_celsius(temp_kelvin):.2f}")
     feels_like_kelvin = response['main']['feels_like']
     feels_like_celsius = kelvin_to_celsius(feels_like_kelvin)
     wind_speed = response['wind']['speed']
@@ -33,18 +50,17 @@ def check_weather():
     description = response['weather'][0]['description']
     sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
     sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+    
+    print("get all information from openweather API")
 
-    print(f"Temperature in {CITY}: {temp_celsius:.2f}°C")
-    print(f"Temperature in {CITY} feels like: {feels_like_celsius:.2f}°C")
-    print(f"Humidity in {CITY} : {humidity}%")
-    print(f"Wind Speed in {CITY} : {wind_speed}km/h")
-    print(f"General weather in {CITY} : {description}")
-    print(f"Sun rises in {CITY} at {sunrise_time} local time")
-    print(f"Sun Sets in {CITY} at {sunset_time} local time")
-    print(response['sys']['sunset'] + response['timezone'])
-    print(response['sys']['sunrise'] + response['timezone'])
+    # execute and commit datas to database
+    command = f"INSERT INTO ((table name)) VALUES ({temp_celsius}, {humidity}, {wind_speed}, '{description}', '{sunrise_time}', '{sunset_time}')"
+    cursor.execute(command)
+    cnx.commit()
 
-schedule.every(5).seconds.do(check_weather)
+# run main function every 24hours
+schedule.every(24).hours.do(main)
 
+# run schedule function
 while True:
     schedule.run_pending()
